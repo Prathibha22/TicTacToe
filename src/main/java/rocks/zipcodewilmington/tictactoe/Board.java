4fdd126b287203d00350c3e0be0a7aa1b7f7ad52
Prathibha <prathibha@Zipcoders-MacBook-Pro-8.local>
2022-11-06 10:38:46 -0500

    modified with better solution

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -12,22 +12,6 @@ public class Board {
     }
 
     public Boolean isInFavorOfX() {
-
-//        for(int i=0;i<3;i++){
-//            int CountXRow=0;
-//            int CountXCol=0;
-//            for(int j=0;j<3;j++){
-//                if(_matrix[i][j]=='X'){
-//                    CountXRow++;
-//                }
-//                if(_matrix[j][i]=='X'){
-//                    CountXCol++;
-//                }
-//                if(CountXRow ==3 || CountXCol==3){
-//                    return true;
-//                }
-//            }
-//        }
         String status=getWinner();
         if(status=="X")
             return  true;
@@ -35,22 +19,6 @@ public class Board {
     }
 
     public Boolean isInFavorOfO() {
-//        for(int i=0;i<3;i++){
-//            int CountORow=0;
-//            int CountOCol=0;
-//            for(int j=0;j<3;j++){
-//                if(_matrix[i][j]=='O'){
-//                    CountORow++;
-//                }
-//                if(_matrix[j][i]=='O'){
-//                    CountOCol++;
-//                }
-//                if(CountORow ==3 || CountOCol==3){
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
         String status=getWinner();
         if(status=="O")
             return  true;
@@ -70,12 +38,16 @@ public class Board {
         StringBuilder sbDiag1=new StringBuilder() ;
         StringBuilder sbDiag2=new StringBuilder() ;
         ArrayList<String> al=new ArrayList<String>();
+        int matLength=2;
         for(int i=0;i<3;i++) {
             for (int j = 0; j < 3; j++) {
                 sbRow.append(_matrix[i][j]);
                 sbCol.append(_matrix[j][i]);
                 if(i==j){
                     sbDiag1.append(_matrix[i][j]);
+                }
+                if(i+j==matLength){
+                    sbDiag2.append(_matrix[matLength-j][j]);
                 }
             }
             al.add(sbRow.toString());
@@ -84,12 +56,8 @@ public class Board {
             sbCol.delete(0,sbCol.length());
         }
         al.add(sbDiag1.toString());
-        sbDiag2.append(_matrix[0][2]);
-        sbDiag2.append(_matrix[1][1]);
-        sbDiag2.append(_matrix[2][0]);
         al.add(sbDiag2.toString());
-        for(String str:al)
-            System.out.println(str);
+
         for(String str:al) {
             if (str.equals("XXX"))
                 return "X";
